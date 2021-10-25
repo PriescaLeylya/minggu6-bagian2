c577f0577b8aa335ea0d3fe88102dadfecb837f1
PriescaLeylya <priescaleylya@gmail.com>
2021-10-25 13:39:23 +0700

    UTS

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Student;
+use App\Models\User;
 
-class StudentController extends Controller
+
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,8 +15,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        $students = Student::all();
-        return view('students.index',['student'=>$students]);
+        $users = User::all();
+        return view('users.index',['user'=>$users]);
     }
 
     /**
@@ -25,7 +26,7 @@ class StudentController extends Controller
      */
     public function create()
     {
-        return view('students.create');
+        return view('users.create');
     }
 
     /**
@@ -37,10 +38,10 @@ class StudentController extends Controller
     public function store(Request $request)
     {
         //add data
-        Student::create($request->all());
+        User::create($request->all());
         // if true, redirect to index
-        return redirect()->route('students.index')
-            ->with('success', 'Add data success!');
+        return redirect()->route('users.index')
+        ->with('success', 'Add data success!');
     }
 
     /**
@@ -51,8 +52,8 @@ class StudentController extends Controller
      */
     public function show($id)
     {
-        $student = Student::find($id);
-        return view('students.show',['student'=>$student]);
+        $user = User::find($id);
+        return view('users.show',['user'=>$user]);
     }
 
     /**
@@ -63,9 +64,8 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
-        $student = Student::find($id);
-        return view('students.edit',['student'=>$student]);
-
+        $user = User::find($id);
+        return view('users.edit',['user'=>$user]);
     }
 
     /**
@@ -77,14 +77,13 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $student = Student::find($id);
-        $student->nim = $request->nim;
-        $student->name = $request->name;
-        $student->class = $request->class;
-        $student->department = $request->department;
-        $student->phone_number = $request->phone_number;
-        $student->save();
-        return redirect()->route('students.index');
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->password = $request->password;
+        $user->email = $request->email;
+        $user->save();
+        return redirect()->route('users.index');
     }
 
     /**
@@ -95,9 +94,9 @@ class StudentController extends Controller
      */
     public function destroy($id)
     {
-        $student = Student::find($id);
-        $student->delete();
-        return redirect()->route('students.index');
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('users.index');
     }
 
     public function search(Request $request)
